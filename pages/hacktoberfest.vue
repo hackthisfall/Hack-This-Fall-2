517efe7f8adbea503bf52fa10894ce651fc7c16b
Apoorv Dwivedi <apoorvd14@gmail.com>
2022-10-03 21:01:54 +0530

    responsive hacktoberfest page

--- pages/hacktoberfest.vue
+++ pages/hacktoberfest.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="container">
     <img class="htf" alt="htf" src="~/assets/Hacktoberfest/HTFLogo.png" />
-    <img class="solana" alt="solana" src="~/assets/Hacktoberfest/SolanaLogo.png" />
+    <img
+      class="solana"
+      alt="solana"
+      src="~/assets/Hacktoberfest/SolanaLogo.png"
+    />
     <img class="grid" alt="grid" src="~/assets/Hacktoberfest/Grid.png" />
-    <img class="section" alt="middle section" src="~/assets/Hacktoberfest/Section.png" />
+    <img
+      class="section"
+      alt="middle section"
+      src="~/assets/Hacktoberfest/Section.png"
+    />
+    <img
+      class="section-mobile"
+      alt="middle section"
+      src="~/assets/Hacktoberfest/SectionMobile.png"
+    />
   </div>
 </template>
 
-<style>
+<style lang="scss">
 .container {
   height: 100vh;
   width: 100vw;
@@ -21,15 +34,44 @@
 }
 
 .grid {
-    height: 100%;
+  height: 100%;
+  @include respond-below(mobile) {
+    height: 80%;
+  }
 }
 
 .section {
-    position: absolute;
-    width: 85vw;
-    left: 50%;
-    transform: translate(-50%, 0);
-    top: 0;
+  position: absolute;
+  width: 50vw;
+  left: 50%;
+  transform: translate(-50%, 0);
+  top: 25%;
+  
+  @include respond-below(mobile) {
+    display: none;
+  }
+
+  @include respond-below(xs) {
+    top: 25%;
+  }
+
+  @include respond-below(sm) {
+    width: 80vw;
+    top: 30%;
+  }
+  
+  @include respond-below(lg) {
+    width: 70vw;
+  }
+}
+
+.section-mobile {
+  display: none;
+  position: absolute;
+  @include respond-below(mobile) {
+    display: unset;
+    width: 100vw;
+  }
 }
 
 .htf {
@@ -37,6 +79,15 @@
   top: 0;
   left: 50px;
   width: 145px;
+  @include respond-below(mobile) {
+    width: 79px;
+    left: 31px;
+  }
+
+  @include respond-below(xs) {
+    width: 100px;
+    left: 35px;
+  }
 }
 
 .solana {
@@ -44,6 +95,16 @@
   top: 52px;
   right: 50px;
   width: 201px;
+  @include respond-below(mobile) {
+    width: 130px;
+    top: 23px;
+    right: 31px;
+  }
+  @include respond-below(xs) {
+    width: 138px;
+    top: 23px;
+    right: 35px;
+  }
 }
 
 #mlh-trust-badge {
